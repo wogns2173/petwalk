62240eac603b05a65af95bf343279248a85a9a1f
KangKyeongSeok <753951zxc@naver.com>
2023-05-02 09:24:12 +0900

    routeMateList

--- src/main/java/com/pet/walkroute/controller/MateFindController.java
+++ src/main/java/com/pet/walkroute/controller/MateFindController.java
@@ -1,25 +1,36 @@
 package com.pet.walkroute.controller;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pet.walkroute.dao.MateFindDAO;
+import com.pet.walkroute.service.MateFindService;
+
 
 @Controller
 public class MateFindController {
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@RequestMapping(value = "/matefind")
+	@Autowired MateFindService service;
+	
+	@RequestMapping(value = "/matefind/list")
 	public String home(Locale locale, Model model) {
-		model.addAttribute("msg", "matefind");
-		return "home";
+		ArrayList<MateFindDAO> dong = service.dong();
+		logger.info("dong : " + dong);
+		model.addAttribute("list",dong);
+		
+		return "map";
 	}
 	
 }
--- src/main/java/com/pet/walkroute/dao/MateFindDAO.java
+++ src/main/java/com/pet/walkroute/dao/MateFindDAO.java
@@ -1,5 +1,9 @@
 package com.pet.walkroute.dao;
 
+import java.util.ArrayList;
+
 public interface MateFindDAO {
+
+	ArrayList<MateFindDAO> dong();
 
 }
