0e5199d954e74d9b901df820ba551dfb719f2f08
leechanghoon <rckdgns125@naver.com>
2023-05-09 09:55:51 +0900

    .

--- src/main/java/com/pet/member/dto/MemberDTO.java
+++ src/main/java/com/pet/member/dto/MemberDTO.java
@@ -3,14 +3,72 @@ package com.pet.member.dto;
 public class MemberDTO {
 
 	private String userID;
+	private int siID;
+	private int guID;
+	private int dongID;
+	private String siName;
+	private String guName;
+	private String dongName;
 	private String userPW;
 	private String userNickname;
 	private String userName;
 	private int userAge;
+	private String userBirthdate;
 	private String userGender;
 	private String userContact;
-	private String userEmail;
+	private String userEmail;	
+	private String Role;
 	
+
+
+	public String getRole() {
+		return Role;
+	}
+	public void setRole(String role) {
+		Role = role;
+	}
+	public String getSiName() {
+		return siName;
+	}
+	public void setSiName(String siName) {
+		this.siName = siName;
+	}
+	public String getGuName() {
+		return guName;
+	}
+	public void setGuName(String guName) {
+		this.guName = guName;
+	}
+	public String getDongName() {
+		return dongName;
+	}
+	public void setDongName(String dongName) {
+		this.dongName = dongName;
+	}
+	public int getSiID() {
+		return siID;
+	}
+	public void setSiID(int siID) {
+		this.siID = siID;
+	}
+	public int getGuID() {
+		return guID;
+	}
+	public void setGuID(int guID) {
+		this.guID = guID;
+	}
+	public int getDongID() {
+		return dongID;
+	}
+	public void setDongID(int dongID) {
+		this.dongID = dongID;
+	}
+	public String getUserBirthdate() {
+		return userBirthdate;
+	}
+	public void setUserBirthdate(String userBirthdate) {
+		this.userBirthdate = userBirthdate;
+	}
 	public String getUserID() {
 		return userID;
 	}
